9cf9008ab0be59fb62ef6304f8daa3320d0c0363
Amit Rai <techamitn@gmail.com>
2018-07-26 00:03:15 +0530

    DataBinding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatListModule } from '@angular/material/list';
@@ -29,6 +29,7 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatButtonModule,
     FlexLayoutModule,
     MatListModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
